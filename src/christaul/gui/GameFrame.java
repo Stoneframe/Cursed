01cff5012a602b1ce598ca7ced5e68b83acd8998
Paul Steneram Bibby <paul.steneram.bibby@gmail.com>
2019-11-12 18:51:52 +0100

    Close window on Escape

--- src/christaul/gui/GameFrame.java
+++ src/christaul/gui/GameFrame.java
@@ -1,6 +1,13 @@
 package christaul.gui;
 
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.KeyStroke;
+
+import com.sun.glass.events.KeyEvent;
 
 public class GameFrame
 	extends JFrame
@@ -10,6 +17,24 @@ public class GameFrame
 	public GameFrame()
 	{
 		super("Cursed");
+
+		KeyStroke escapeKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0, false);
+
+		getRootPane()
+			.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
+			.put(escapeKeyStroke, "ESCAPE");
+		getRootPane()
+			.getActionMap()
+			.put("ESCAPE", new AbstractAction()
+			{
+				private static final long serialVersionUID = 4403370472658630695L;
+
+				@Override
+				public void actionPerformed(ActionEvent e)
+				{
+					System.exit(0);
+				}
+			});
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
