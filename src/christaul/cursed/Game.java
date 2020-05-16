cb8272fb5cfc037a6474b08008d12845d61cb732
Colactic <Christian.S.Bibby@gmail.com>
2020-05-16 16:36:53 +0200

    Added Game Class

---
+++ src/christaul/cursed/Game.java
@@ -0,0 +1,14 @@
+package christaul.cursed;
+
+import christaul.cursed.display.Display;
+
+public class Game
+{
+	private Display display;
+	
+	public Game() 
+	{
+		display = new Display();
+	}
+
+}
--- src/christaul/cursed/Launcher.java
+++ src/christaul/cursed/Launcher.java
@@ -1,11 +1,9 @@
 package christaul.cursed;
 
-import christaul.cursed.display.Display;
-
 public class Launcher
 {
 	public static void main(String[] args)
 	{
-		new Display();
+		new Game();
 	}
 }
