85f1176646f32640bde1038d249a00d15e68e3b2
Paul Steneram Bibby <paul.steneram.bibby@gmail.com>
2020-05-20 17:51:28 +0200

    Refactor runGame test

--- tests/christaul/cursed/GameTest.java
+++ tests/christaul/cursed/GameTest.java
@@ -1,30 +1,75 @@
 package christaul.cursed;
 
+import static org.junit.Assert.assertEquals;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GameTest
 {
+	private static final Duration DURATION = Duration.ofMillis(100);
+
+	private TestableGame game;
+
+	private LocalTime startTime;
+	private LocalTime stopTime;
+
 	@BeforeEach
 	public void setUp() throws Exception
 	{
+		game = new TestableGame();
 	}
 
 	@AfterEach
 	public void tearDown() throws Exception
 	{
+		game = null;
+		startTime = null;
+		stopTime = null;
 	}
 
 	@Test
 	public void runGame() throws Exception
 	{
-		TestableGame game = new TestableGame();
+		givenNothing();
+		thenStartGame();
+		thenWait(DURATION);
+		thenStopGame();
+		expectGameRan(DURATION);
+	}
 
+	private void givenNothing()
+	{
+	}
+
+	private void thenStartGame()
+	{
 		game.start();
+		startTime = LocalTime.now();
+	}
 
-		Thread.sleep(100);
+	private void thenWait(Duration duration) throws InterruptedException
+	{
+		Thread.sleep(duration.toMillis());
+	}
 
+	private void thenStopGame()
+	{
 		game.stop();
+		stopTime = LocalTime.now();
+	}
+
+	private void expectGameRan(Duration expectedDuration)
+	{
+		assertEquals(getTotalGameRunTime().toMillis(), expectedDuration.toMillis(), 10);
+	}
+
+	private Duration getTotalGameRunTime()
+	{
+		return Duration.between(startTime, stopTime);
 	}
 }
